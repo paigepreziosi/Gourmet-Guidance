e61dc40a5a27677985c6a7f61d60bc83e4c816a4
Kalleepar <kalleepar@gmail.com>
2024-02-01 14:01:50 -0500

    linked store data to table

--- models/Store.js
+++ models/Store.js
@@ -3,6 +3,38 @@ const sequelize = require('../config/connection');
 
 class Store extends Model {}
 
-Project.init()
+Project.init(
+    {
+    id: {  
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    storename: {
+        type: DataTypes.STRING,
+        allowNull: false,
+     },
+    chef_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    eta: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    menu_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'menu',
+            key: 'id',
+        },
+    },
+    },
+)
 
-module.exports = Project;
+module.exports = Store;
