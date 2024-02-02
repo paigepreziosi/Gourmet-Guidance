b7e00d3afe4d9dc97c642943ebb2e1d66e9f9577
unknown <vanzandt.school.email@gmail.com>
2024-02-02 12:04:38 -0500

    Updated handlebars

--- config/routes.js
+++ config/routes.js
@@ -3,26 +3,26 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-       res.render('all')
+       res.render('first')
   } catch (err) {
       res.status(500).json(err)
   }
 })
 
 router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-      res.redirect('/');
-      return;
-  }
+  // if (req.session.logged_in) {
+  //     res.redirect('/');
+  //     return;
+  // }
   res.render('login');
 });
 
 router.get('/signup', (req, res) => {
-  if (req.session.logged_in) {
-      res.redirect('/');
-      return;
-  }
-  res.render('signUp');
+  // if (req.session.logged_in) {
+  //     res.redirect('/');
+  //     return;
+  // }
+  res.render('signup');
 });
 
 module.exports = router;
