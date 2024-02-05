7a5f782c468d8140b67c8ccadffa5e2a3e25eb8e
Paige Preziosi <paigepreziosi@Paiges-Laptop.lan>
2024-02-05 11:35:16 -0500

    working signup

--- public/public/js/signup.js
+++ public/public/js/signup.js
@@ -1,46 +1,38 @@
-// Sign up form 
+
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-    // hideSignUpAlert();
+  event.preventDefault();
+  
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if(!username || !password || password.length < 8) 
-    // {signUpAlert()}
-  
-    if (username && password && password.length > 8) {
+  const username = document.querySelector('#username-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+
+  if (!username || !password || password.length < 8) {
+
+  }
+
+  if (username && password && password.length >= 8) {
       const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
+          method: 'POST',
+          body: JSON.stringify({ username, password }),
+          headers: { 'Content-Type': 'application/json' },
       });
-  
-    // This automatically logs the user in after a successful sign up
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        // signUpAlert();
-      }
-    }
-  };
-
-  // const signUpAlert = () => {
-  //   const signUpAlert = document.querySelector('#signUpAlert');
-  //   signUpAlert.style.display = 'block';
-  // }
-  
-  // const hideSignUpAlert = () => {
-  //   const signUpAlert = document.querySelector('#signUpAlert');
-  //   signUpAlert.style.display = 'none';
-  // }
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
-
-  document
-    .querySelector('#signupBtn')
-    .addEventListener('submit', signupFormHandler);
 
     
+      if (response.ok) {
+          document.location.replace('/');
+      } else {
+        
+      }
+  }
+};
+
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
+
+
+document
+  .querySelector('#signupBtn')
+  .addEventListener('click', signupFormHandler);
